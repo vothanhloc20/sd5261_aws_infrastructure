95ba55b90ba3f0d5118f91640b7d9e77c8574baf
Nguyễn Đặng Hiếu <vothanhloc20@gmail.com>
2023-08-22 21:14:50 +0700

    update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,8 @@ provider "azurerm" {
 
 module "resource_group" {
     source = "./modules/resource_group"
+    resource_group_name     = var.resource_group_name
+    resource_group_location = var.resource_group_location
 }
 
 module "vnet" {
